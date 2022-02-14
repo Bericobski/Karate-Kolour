8a3f82cda1e73c3a925d2863c230b16f8256722d
MMM324 <75086725+MMM324@users.noreply.github.com>
2022-02-14 14:28:00 -0300

    Prueba
    prueba

---
+++ Enemies.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Enemies : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    public Vector2 playerpos;
+    public float speed = 3f;
+    public int enemy_ID;
+    public SpriteRenderer cube;
+    public player_char karateka;
+
+    void Start()
+    {
+        enemy_ID = Random.Range(0, 4);
+        speed = Random.Range(3f, 6f);
+        player = GameObject.FindGameObjectWithTag("player");
+    }
+
+    void Update()
+    {
+        //SetSpedd();
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        posture_check();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Deflected by" + other.name);
+        if (other.tag == "player")
+        {
+            player_char karateka = other.GetComponent<player_char>();
+            if (karateka != null)
+            {
+                if (karateka.posture_ID == enemy_ID)
+                {
+                    Destroy(this.gameObject);
+                    karateka.score = karateka.score + 1;
+                }
+                else if (karateka.posture_ID != enemy_ID)
+                {
+                    karateka.dead();
+                }
+            }
+        }
+    }
+
+    public void posture_check()
+    {
+        switch (enemy_ID)
+        {
+            case 0:
+                cube.color = new Color(1, 0.5f, 0);
+                break;
+            case 1:
+                cube.color = new Color(0, 1, 0);
+                break;
+            case 2:
+                cube.color = new Color(1, 0, 1);
+                break;
+            case 3:
+                cube.color = new Color(1, 1, 0);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    /*public void SetSpedd() 
+    {
+        if (karateka != null)
+        {
+            player_char karateka = gameObject.GetComponent<player_char>();
+            if (karateka.score >= 5 && karateka.score <= 10)
+            {
+                speed = Random.Range(7f, 10f);
+            }
+            else if (karateka.score > 10) { speed = Random.Range(7f, 10f); }
+        }
+        else { Debug.Log("No funca");  }
+        //El problema seria que no reonoce al karateka por lo tanto lo toma como un nulo.
+    }*/
+
+
+}
---
+++ Spawn_manager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_manager : MonoBehaviour
+{
+    public player_char points;
+    public GameObject Enemy_Prefab;
+    public GameObject[] Spawners;
+    public bool isSpawningEnemies;
+    public int[] cantEnemies;
+    public int enemycounter;
+    public int waiTime = 5;
+
+    public enum actualEnemiesState
+    {
+        easy, 
+        normal, 
+        hard
+    }
+
+    public actualEnemiesState state;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+        state = actualEnemiesState.easy;
+    }
+    public void WaitTIme(actualEnemiesState state)
+    {
+        switch (state)
+        {
+            case actualEnemiesState.normal:
+                waiTime = 3;
+                break;
+            case actualEnemiesState.hard:
+                waiTime = 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (isSpawningEnemies)
+        {
+
+            for (int i = 0; i < cantEnemies[(int)state]; i++)
+            {
+                Instantiate(Enemy_Prefab, new Vector3(Random.Range(10f, -10f), Random.Range(8f, 15f), 0), Quaternion.identity);
+                enemycounter++;
+                if (points.score >= 5 && points.score <= 10) {state = actualEnemiesState.normal}
+                else if (points.score > 10) { state = actualEnemiesState.hard; }
+                WaitTIme(state);
+                yield return new WaitForSeconds(waiTime);
+            }
+        }
+    }
+}
+
+
