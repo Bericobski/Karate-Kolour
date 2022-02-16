3b0c99b4cafd05e2c6257ac4f768ba464eecb2c1
Sarduc <franimpe@outlook.es>
2022-02-15 22:38:14 -0300

    Sprites de enemigos
    Si

--- Karate Kolour Game/Assets/Script/Enemies.cs
+++ Karate Kolour Game/Assets/Script/Enemies.cs
@@ -10,20 +10,30 @@ public class Enemies : MonoBehaviour
     public float speed = 3f;
     public int enemy_ID;
     public SpriteRenderer cube;
-    
+    public Animator anim;
+    public int ninja_anim;
+    public int knife_anim;
+    public int beast_anim;
+    public int dragon_anim;
 
     void Start()
     {
         enemy_ID = Random.Range(0, 4);
         speed = Random.Range(3f, 6f);
         player = GameObject.FindGameObjectWithTag("player");
+        ninja_anim = Random.Range(0, 1);
+        knife_anim = Random.Range(2, 3);
+        beast_anim = Random.Range(4, 5);
+        dragon_anim = Random.Range(6, 7);
+        anim_check();
     }
 
     void Update()
     {
         //SetSpedd();
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-        posture_check();
+        //posture_check();
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -46,7 +56,7 @@ public class Enemies : MonoBehaviour
         }
     }
 
-    public void posture_check()
+    /*public void posture_check()
     {
         switch (enemy_ID)
         {
@@ -65,7 +75,7 @@ public class Enemies : MonoBehaviour
             default:
                 break;
         }
-    }
+    }*/
 
     public void SetSpedd() 
     {
@@ -77,6 +87,31 @@ public class Enemies : MonoBehaviour
         }
         else { Debug.Log("No funca");  }
     //El problema seria que no reonoce al karateka por lo tanto lo toma como un nulo.
+    }
+
+
+    public void anim_check()
+    {
+        if (enemy_ID == 0)
+        {
+            if (ninja_anim == 0) { anim.Play("ninja_patada"); }
+            if (ninja_anim == 1) { anim.Play("ninja_nunchaku"); }
+        }
+        if (enemy_ID == 1)
+        {
+            if (knife_anim == 2) { anim.Play("shuriken"); }
+            if (knife_anim == 3) { anim.Play("cuchillo"); }
+        }
+        if (enemy_ID == 2)
+        {
+            if (beast_anim == 4) { anim.Play("snake"); }
+            if (beast_anim == 5) { anim.Play("pez_globo"); }
+        }
+        if (enemy_ID == 3)
+        {
+            if (dragon_anim == 6) { anim.Play("fantasma"); }
+            if (dragon_anim == 5) { anim.Play("fireball"); }
+        }        
     }
 
 }
