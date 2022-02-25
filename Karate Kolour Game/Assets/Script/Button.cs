b583bc23b93f330e9151d1f538887e78911574e4
Sarduc <franimpe@outlook.es>
2022-02-25 15:47:27 -0300

    Cambios de sistema

--- Karate Kolour Game/Assets/Script/Button.cs
+++ Karate Kolour Game/Assets/Script/Button.cs
@@ -5,31 +5,13 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public int button_ID;
-    public GameObject player;
-    public Animator anims;
-    public Sprite[] icono;
+    public player_char player;
 
-    void Start() { }
-    void Update() { }
-    private void OnMouseDown()
+
+  
+    public void click_button()
     {
-        switch (button_ID)
-        {
-            case 0:
-                anims.Play("boton_tigre");
-                break;
-            case 1:
-                anims.Play("boton_gruya");
-                break;
-            case 2:
-                anims.Play("boton_snake");
-                break;
-            case 3:
-                anims.Play("boton_dragon");
-                break;
-            default:
-                break;
-        }
+        player.posture_ID = button_ID;
+        player.play_idle();
     }
-
 }
