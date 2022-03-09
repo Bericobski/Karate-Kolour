c7271872020b32addeedd460d8a12f9cac2e44e2
Sarduc <franimpe@outlook.es>
2022-03-09 15:26:12 -0300

    xd

--- Karate Kolour Game/Assets/Script/UI_Manager.cs
+++ Karate Kolour Game/Assets/Script/UI_Manager.cs
@@ -10,5 +10,5 @@ public class UI_Manager : MonoBehaviour
 
     public void update_score() { puntaje.text = "score: " + game_instance.instance.player_score; } 
 
-    void Update() { update_score(); }
+    void Update() { update_score();}
 }
