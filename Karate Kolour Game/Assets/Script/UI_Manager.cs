61b9f2329a42df513f68c2ec28e7e8f08024ec4a
Sarduc <franimpe@outlook.es>
2022-04-28 22:11:36 -0300

    Boton nuevo

--- Karate Kolour Game/Assets/Script/UI_Manager.cs
+++ Karate Kolour Game/Assets/Script/UI_Manager.cs
@@ -7,6 +7,12 @@ using TMPro;
 public class UI_Manager : MonoBehaviour
 {
     public TextMeshProUGUI puntaje;
+    public string uerreele = "https://imagecampus.itch.io";
+
+    public void open_url()
+    {
+        Application.OpenURL(uerreele);
+    } 
 
     public void update_score() { puntaje.text = "score: " + game_instance.instance.player_score; } 
 
